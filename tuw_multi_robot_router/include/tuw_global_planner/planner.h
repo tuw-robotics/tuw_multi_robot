0873af2b9d953c4bd0095e25a9fc52e70eb56e6a
BinderBenjamin <benjamin@binder.com>
2018-06-12 12:08:08 +0000

    ...

--- tuw_multi_robot_router/include/tuw_global_planner/planner.h
+++ tuw_multi_robot_router/include/tuw_global_planner/planner.h
@@ -38,17 +38,17 @@
 
 namespace multi_robot_router
 {
-    class Planner
-    {
-        public:
-            Planner(const uint32_t _nr_robots);
-            Planner();
+class Planner
+{
+  public:
+    Planner(const uint32_t _nr_robots);
+    Planner();
 
-            /**
+    /**
             * @brief resizes the planner to a different nr of _nr_robots
             */
-            void resize(const uint32_t _nr_robots);
-            /**
+    void resize(const uint32_t _nr_robots);
+    /**
             * @brief generates the plan from (Vertex[odom robotPose] to Vertex[_goals]
             * @param _starts the start positions of all robots 
             * @param _goals the goal positions fo all robots 
@@ -56,109 +56,106 @@ namespace multi_robot_router
             * @param _map the grid_map used to find the start and goal segments of the path
             * @param _graph the full graph of the map used for planning the path
             */
-            bool makePlan(const std::vector< Eigen::Vector3d > &_starts, const std::vector< Eigen::Vector3d > &_goals, const std::vector<float> &_radius, const cv::Mat &_map, const float &_resolution, const Eigen::Vector2d &_origin, const std::vector<Segment> &_graph);
-            /**
+    bool makePlan(const std::vector<Eigen::Vector3d> &_starts, const std::vector<Eigen::Vector3d> &_goals, const std::vector<float> &_radius, const cv::Mat &_map, const float &_resolution, const Eigen::Vector2d &_origin, const std::vector<Segment> &_graph);
+    /**
              * @brief sets the CollisionResolverType used 
              */
-            void setCollisionResolutionType(const SegmentExpander::CollisionResolverType _cr);
-            /**
+    void setCollisionResolutionType(const SegmentExpander::CollisionResolverType _cr);
+    /**
              * @brief returns the found Routing Table
              * @param _robot the robot to whom the routing table belongs to
              */
-            const std::vector<Checkpoint> &getRoute(const uint32_t _robot) const;
-            /**
+    const std::vector<Checkpoint> &getRoute(const uint32_t _robot) const;
+    /**
              * @brief getter
              * @returns the duration of the planning attempt 
              */
-            uint32_t getDuration_ms() const;
-            /**
+    uint32_t getDuration_ms() const;
+    /**
              * @brief getter
              * @returns the Overall path length of the planning attempt 
              */
-            float getOverallPathLength() const;
-            /**
+    float getOverallPathLength() const;
+    /**
              * @brief getter
              * @returns the longest Path length of the planning attempt 
              */
-            float getLongestPathLength() const;
-            /**
+    float getLongestPathLength() const;
+    /**
              * @brief getter
              * @returns the priority reschedule attempts
              */
-            uint32_t getPriorityScheduleAttemps() const;
-            /**
+    uint32_t getPriorityScheduleAttemps() const;
+    /**
              * @brief getter
              * @returns the speed reschedule attempts
              */
-            uint32_t getSpeedScheduleAttemps() const;
+    uint32_t getSpeedScheduleAttemps() const;
 
+    std::shared_ptr<float> potential_;
 
-            std::shared_ptr<float> potential_;
+  private:
+    //find the right segments to start from
+    bool calculateStartPoints(const std::vector<float> &_radius, const cv::Mat &_map, const float &resolution, const Eigen::Vector2d &origin, const std::vector<Segment> &_graph);
+    //Find the segment if the graph is a voronoi one
+    int32_t getSegment(const std::vector<Segment> &_graph, const Eigen::Vector2d &_pose) const;
+    //Helper dist calculation
+    float distanceToSegment(const Segment &_s, const Eigen::Vector2d &_p) const;
+    //Checks if _seg is a leave of the graph and uses the closes neighbor as segment if the width of the leave is to small
+    bool resolveSegment(const std::vector<Segment> &_graph, const uint32_t &_segId, const Eigen::Vector2d &_originPoint, const float &_radius, uint32_t &_foundSeg) const;
+    //for every path remove the first and last "segmentOptimizations_" number of segments if possible
+    void optimizePaths(const std::vector<Segment> &_graph);
+    //adds start and endpoints to the path
+    void postprocessRoutingTable();
+    //Preprocessing endpoints for enpoint calculation threaded
+    bool preprocessEndpoints(const std::vector<float> &_radius, const float &resolution, const Eigen::Vector2d &origin, const std::vector<Segment> &_graph);
+    //Calculate endpoints
+    bool processEndpointsExpander(const cv::Mat &_map, const std::vector<Segment> &_graph, const Eigen::Vector2d &_realStart, const Eigen::Vector2d &_realGoal, Eigen::Vector2d &_voronoiStart, Eigen::Vector2d &_voronoiGoal, uint32_t &_segmentStart, uint32_t &_segmentGoal, const uint32_t _diameter, const uint32_t _index) const;
 
-        private:
-            //find the right segments to start from
-            bool calculateStartPoints(const std::vector<float> &_radius, const cv::Mat &_map, const float &resolution, const Eigen::Vector2d &origin, const std::vector<Segment> &_graph);
-            //Find the segment if the graph is a voronoi one
-            int32_t getSegment(const std::vector<Segment> &_graph, const Eigen::Vector2d &_pose) const;
-            //Helper dist calculation
-            float distanceToSegment(const Segment &_s, const Eigen::Vector2d &_p) const;
-            //Checks if _seg is a leave of the graph and uses the closes neighbor as segment if the width of the leave is to small
-            bool resolveSegment(const std::vector< Segment > &_graph, const uint32_t &_segId, const Eigen::Vector2d &_originPoint, const float &_radius, uint32_t &_foundSeg) const;
-            //for every path remove the first and last "segmentOptimizations_" number of segments if possible
-            void optimizePaths(const std::vector<Segment> &_graph);
-            //adds start and endpoints to the path
-            void postprocessRoutingTable();
-            //Preprocessing endpoints for enpoint calculation threaded
-            bool preprocessEndpoints(const std::vector<float> &_radius, const float &resolution, const Eigen::Vector2d &origin, const std::vector<Segment> &_graph);
-            //Calculate endpoints
-            bool processEndpointsExpander(const cv::Mat &_map, const std::vector<Segment> &_graph, const Eigen::Vector2d &_realStart, const Eigen::Vector2d &_realGoal, Eigen::Vector2d &_voronoiStart, Eigen::Vector2d &_voronoiGoal, uint32_t &_segmentStart, uint32_t &_segmentGoal, const uint32_t _diameter, const uint32_t _index) const;
+    uint32_t robot_nr_;
+    std::vector<Eigen::Vector3d> starts_;
+    std::vector<Eigen::Vector3d> goals_;
+    std::vector<Eigen::Vector2d> realGoals_;
+    std::vector<Eigen::Vector2d> realStart_;
+    std::vector<Eigen::Vector2d> voronoiGoals_;
+    std::vector<Eigen::Vector2d> voronoiStart_;
+    std::vector<uint32_t> startSegments_;
+    std::vector<uint32_t> goalSegments_;
 
-            
-            
-            uint32_t robot_nr_;
-            std::vector<Eigen::Vector3d> starts_;
-            std::vector<Eigen::Vector3d> goals_;
-            std::vector<Eigen::Vector2d> realGoals_;
-            std::vector<Eigen::Vector2d> realStart_;
-            std::vector<Eigen::Vector2d> voronoiGoals_;
-            std::vector<Eigen::Vector2d> voronoiStart_;
-            std::vector<uint32_t> startSegments_;
-            std::vector<uint32_t> goalSegments_;
+    PointExpander pointExpander_;
+    MultiRobotRouter *multiRobotRouter_;
+    MultiRobotRouter mrr_;
+    MultiRobotRouterThreadedSrr mrrTs_;
+    std::vector<std::vector<Checkpoint>> routingTable_;
+    float overallPathLength_;
+    float longestPatLength_;
+    uint32_t duration_;
 
-            PointExpander pointExpander_;
-            MultiRobotRouter *multiRobotRouter_;
-            MultiRobotRouter mrr_;
-            MultiRobotRouterThreadedSrr mrrTs_;
-            std::vector<std::vector<Checkpoint>> routingTable_;
-            float overallPathLength_;
-            float longestPatLength_;
-            uint32_t duration_;
-
-        protected:
-            enum class goalMode
-            {
-                use_segment_goal,
-                use_voronoi_goal,
-                use_map_goal
-            };
-            enum class graphType
-            {
-                voronoi,
-                random
-            };
-            enum class routerType
-            {
-                singleThread,
-                multiThreadSrr
-            };
-            graphType graphMode_ = graphType::voronoi;
-            goalMode goalMode_ = goalMode::use_voronoi_goal;
-            float routerTimeLimit_s_ = 10.0;
-            bool segmentOptimizations_= false;
-            bool speedRescheduling_ = true;
-            bool priorityRescheduling_ = true;
-                         
-            void setPlannerType(routerType _type, uint32_t _nr_threads);
+  protected:
+    enum class goalMode
+    {
+        use_segment_goal,
+        use_voronoi_goal,
+        use_map_goal
     };
-}
+    enum class graphType
+    {
+        voronoi,
+        random
+    };
+    enum class routerType
+    {
+        singleThread,
+        multiThreadSrr
+    };
+    graphType graphMode_ = graphType::voronoi;
+    goalMode goalMode_ = goalMode::use_voronoi_goal;
+    float routerTimeLimit_s_ = 10.0;
+    bool segmentOptimizations_ = false;
+    bool speedRescheduling_ = true;
+    bool priorityRescheduling_ = true;
+
+    void setPlannerType(routerType _type, uint32_t _nr_threads);
+};
+} // namespace multi_robot_router
 #endif // PLANNER_H
--- tuw_multi_robot_router/include/tuw_global_planner/point_expander.h
+++ tuw_multi_robot_router/include/tuw_global_planner/point_expander.h
@@ -40,15 +40,15 @@
 
 namespace multi_robot_router
 {
-    class PointExpander
-    {
-        public:
-            /**
+class PointExpander
+{
+  public:
+    /**
              * @brief initializes the point expander with a distance map
              * @param _map a cv::Mat containing a distance_transformed map (e.g. opencv disttransform)
              */
-            void initialize(const cv::Mat &_map);
-            /** 
+    void initialize(const cv::Mat &_map);
+    /** 
              * @brief searches the first occurence of a point in the goals_ list and returns the index
              * @details used to find the closes segment to the start point. The goals are usually the centerpoints of the considered segments with the segment id. If a path is allready found the expander can be started again to find the nth furthest segment to optimize the path.  
              * @param _start the start point on the map
@@ -61,90 +61,89 @@ namespace multi_robot_router
              * @param _radius the robot _radius
              * @returns true if a point was found
              */
-            bool findGoalOnMap(const Eigen::Vector2d &_start, const uint32_t &_cycles, float *_potential, const std::map<uint32_t, Eigen::Vector2d> &_goals, const uint32_t &_optimizationSteps, Eigen::Vector2d &_foundPoint, int32_t &_segIdx, const uint32_t &_radius);
-            /**
+    bool findGoalOnMap(const Eigen::Vector2d &_start, const uint32_t &_cycles, float *_potential, const std::map<uint32_t, Eigen::Vector2d> &_goals, const uint32_t &_optimizationSteps, Eigen::Vector2d &_foundPoint, int32_t &_segIdx, const uint32_t &_radius);
+    /**
              * @brief returns the distance to the closest obstacle for a point
              * @param vec the point which is used to calculate the distance to the closest obstacle 
              */
-            float getDistanceToObstacle(const Eigen::Vector2d &vec);
-        private:
-            template <class T, class S, class C>
-            void clearpq(std::priority_queue<T, S, C> &q)
-            {
-                q = std::priority_queue<T, S, C>();
-            }
-            class Index
-            {
-                public:
+    float getDistanceToObstacle(const Eigen::Vector2d &vec);
 
-                    Index(int index, float c, float d, float p)
-                    {
-                        i = index;
-                        weight = c;
-                        dist = d;
-                        potential = p;
-                    }
+  private:
+    template <class T, class S, class C>
+    void clearpq(std::priority_queue<T, S, C> &q)
+    {
+        q = std::priority_queue<T, S, C>();
+    }
+    class Index
+    {
+      public:
+        Index(int index, float c, float d, float p)
+        {
+            i = index;
+            weight = c;
+            dist = d;
+            potential = p;
+        }
 
-                    Index(int x_val, int y_val, int nx, float c, float d, float p)
-                    {
-                        i = x_val + y_val * nx;
-                        weight = c;
-                        dist = d;
-                        potential = p;
-                    }
+        Index(int x_val, int y_val, int nx, float c, float d, float p)
+        {
+            i = x_val + y_val * nx;
+            weight = c;
+            dist = d;
+            potential = p;
+        }
 
-                    Index offsetDist(int dist_x, int dist_y, int nx, int ny)
-                    {
-                        int x_val = (i % nx) + dist_x;
-                        int y_val = (i / nx) + dist_y;
+        Index offsetDist(int dist_x, int dist_y, int nx, int ny)
+        {
+            int x_val = (i % nx) + dist_x;
+            int y_val = (i / nx) + dist_y;
 
-                        if(x_val < 0 || x_val > nx || y_val < 0 || y_val > ny)
-                            return Index(-1, -1, -1, -1);
+            if (x_val < 0 || x_val > nx || y_val < 0 || y_val > ny)
+                return Index(-1, -1, -1, -1);
 
-                        return Index(x_val, y_val, nx, 0, 0, 0);
-                    }
+            return Index(x_val, y_val, nx, 0, 0, 0);
+        }
 
-                    int getX(int nx)
-                    {
-                        return (i % nx);
-                    }
+        int getX(int nx)
+        {
+            return (i % nx);
+        }
 
-                    int getY(int nx)
-                    {
-                        return (i / nx);
-                    }
+        int getY(int nx)
+        {
+            return (i / nx);
+        }
 
-                    float distance(Index _p, int _nx)
-                    {
-                        float dx = abs(getX(_nx) - _p.getX(_nx));
-                        float dy = abs(getY(_nx) - _p.getY(_nx));
+        float distance(Index _p, int _nx)
+        {
+            float dx = abs(getX(_nx) - _p.getX(_nx));
+            float dy = abs(getY(_nx) - _p.getY(_nx));
 
-                        return std::sqrt(dx * dx + dy * dy);
-                    }
+            return std::sqrt(dx * dx + dy * dy);
+        }
 
-                    int i;
-                    float weight;
-                    float dist;
-                    float cost;
-                    float potential;
-            };
-
-            struct greater1
-            {
-                bool operator()(const Index &a, const Index &b) const
-                {
-                    return a.weight > b.weight;
-                }
-            };
-            std::priority_queue<Index, std::vector<Index>, greater1> queue_;
-            int nx_, ny_, ns_;
-            Index findGoal(const Index &_start, const uint32_t &_cycles, float *_potential, const std::map<uint32_t, Index> &_goals, const uint32_t &_optimizationSteps, int32_t &segIdx, const uint32_t &_radius);
-            void addPotentialExpansionCandidate(Index _current, int32_t _next_x, int32_t _next_y, float *_potential, uint32_t _distToObstacle);
-            bool isGoal(Index _p, const std::map<uint32_t, Index> &_goals, int32_t &segIdx);
-
-            cv::Mat distance_field_;
-            float neutral_cost_ = 1;
-
+        int i;
+        float weight;
+        float dist;
+        float cost;
+        float potential;
     };
-}
+
+    struct greater1
+    {
+        bool operator()(const Index &a, const Index &b) const
+        {
+            return a.weight > b.weight;
+        }
+    };
+    std::priority_queue<Index, std::vector<Index>, greater1> queue_;
+    int nx_, ny_, ns_;
+    Index findGoal(const Index &_start, const uint32_t &_cycles, float *_potential, const std::map<uint32_t, Index> &_goals, const uint32_t &_optimizationSteps, int32_t &segIdx, const uint32_t &_radius);
+    void addPotentialExpansionCandidate(Index _current, int32_t _next_x, int32_t _next_y, float *_potential, uint32_t _distToObstacle);
+    bool isGoal(Index _p, const std::map<uint32_t, Index> &_goals, int32_t &segIdx);
+
+    cv::Mat distance_field_;
+    float neutral_cost_ = 1;
+};
+} // namespace multi_robot_router
 #endif // VORONOI_EXPANDER_H
