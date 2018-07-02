820d347f694d0dd0748e5019e90fe8412277e19e
Florian Beck <florian.beck@tuwien.ac.at>
2018-07-02 15:37:22 +0200

    added fake pose estimation nodelet

---
+++ tuw_multi_robot_ctrl/include/fake_pose_estimation/fake_pose_estimation_nodelet.h
@@ -0,0 +1,16 @@
+#include <nodelet/nodelet.h>
+#include <ros/ros.h>
+#include <nav_msgs/Odometry.h>
+
+class FakePoseEstimationNodelet : public nodelet::Nodelet
+{
+public:
+  virtual void onInit();
+  
+private:
+  ros::NodeHandle nh_;
+  ros::Publisher pose_pub_;
+  ros::Subscriber odom_sub_;
+  
+  void odomCallback(const nav_msgs::OdometryConstPtr& odom);
+};
---
+++ tuw_multi_robot_ctrl/src/fake_pose_estimation_nodelet.cpp
@@ -0,0 +1,24 @@
+#include "fake_pose_estimation/fake_pose_estimation_nodelet.h"
+#include <pluginlib/class_list_macros.h>
+#include <geometry_msgs/PoseWithCovarianceStamped.h>
+
+PLUGINLIB_EXPORT_CLASS(FakePoseEstimationNodelet, nodelet::Nodelet)
+
+void FakePoseEstimationNodelet::onInit()
+{
+  nh_ = getPrivateNodeHandle();
+  
+  pose_pub_ = nh_.advertise<geometry_msgs::PoseWithCovarianceStamped>("pose", 1);
+  
+  odom_sub_ = nh_.subscribe<nav_msgs::Odometry>("odom", 1, &FakePoseEstimationNodelet::odomCallback, this);
+}
+
+void FakePoseEstimationNodelet::odomCallback(const nav_msgs::OdometryConstPtr& odom)
+{
+  geometry_msgs::PoseWithCovarianceStamped pose;
+  pose.header = odom->header;
+  pose.pose.pose = odom->pose.pose;
+  pose.pose.covariance = odom->pose.covariance;
+  
+  pose_pub_.publish(pose);
+}
